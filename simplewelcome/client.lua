ccecb76e47eece5fe60970936ddb399788c48f0f
ThatZiv <35277113+ThatZiv@users.noreply.github.com>
2018-01-30 19:13:43 -0500

    Add files via upload

---
+++ simplewelcome/client.lua
@@ -0,0 +1,3 @@
+AddEventHandler("playerSpawned", function(spawn)
+    TriggerEvent("chatMessage", "", { 49, 196, 157 }, "Welcome to The ZivinityGaming Freeroam Server. Join our discord at ^1https://discord.gg/yWddFpQ")
+end)
