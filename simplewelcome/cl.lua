468e197bc48fba6affbbb5bee7ac4488610d056a
ThatZiv <35277113+ThatZiv@users.noreply.github.com>
2018-07-03 15:30:39 -0400

    Changed..V.2

--- simplewelcome/cl.lua
+++ simplewelcome/cl.lua
@@ -1,3 +1,10 @@
 AddEventHandler("playerSpawned", function(spawn)
-    TriggerEvent("chatMessage", "", { 49, 196, 157 }, "Welcome to The ZivinityGaming Freeroam Server. Join our discord at ^1https://discord.gg/yWddFpQ")
+   helpText("~b~Welcome ~g~".. GetPlayerName(source) .. " ~b~to Zua's Server")
 end)
+
+function helpText(string)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(string)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
